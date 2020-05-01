8b6873a2ce6c6b292e2db826cb70af81f2059a2b
abinapier <abi.napier@yahoo.com>
2020-04-30 20:32:05 -0600

    new index page

--- web/index.php
+++ web/index.php
@@ -1,5 +1,18 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>Abi Napier Homepage</title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+<body>
+	<header>
+		<nav>
+			<?php 
+		        include $_SERVER['DOCUMENT_ROOT'] . '/acme/common/header.php';
+		    ?>
+		</nav>
+	</header>
 
-  phpinfo();
-
-?>
+</body>
+</html>
