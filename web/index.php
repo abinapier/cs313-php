0ec632b9614bb33d29fd02bb21f8753cc6680bd7
abinapier <abi.napier@yahoo.com>
2020-04-30 21:10:07 -0600

    js added to html

--- web/index.php
+++ web/index.php
@@ -4,6 +4,7 @@
 	<title>Abi Napier Homepage</title>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<script src="js/basic.js"></script>
 </head>
 <body>
 	<header>
