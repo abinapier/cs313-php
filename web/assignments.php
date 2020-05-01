f58d58704122fdd7e37a04be6e75de6dfbf782a7
abinapier <abi.napier@yahoo.com>
2020-04-30 21:01:26 -0600

    assignment page

---
+++ web/assignments.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>Abi Napier Assignments</title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+<body>
+	<header>
+		<nav>
+			<?php 
+		        include $_SERVER['DOCUMENT_ROOT'] . '/common/nav.php';
+		    ?>
+		</nav>
+		<h1>Assign<br>Ments</h1>
+	</header>
+	<main>
+		<h2>CSE 341 Spring 2020:</h2>
+		<ul>
+			<li>Coming Soon</li>
+		</ul>
+	</main>
+	<footer>
+		<?php 
+	        include $_SERVER['DOCUMENT_ROOT'] . '/common/footer.php';
+	    ?>
+	</footer>
+
+</body>
+</html>
