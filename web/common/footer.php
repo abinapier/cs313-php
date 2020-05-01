2edc42cd7db97d34f55bad317d05005e7ff93931
abinapier <abi.napier@yahoo.com>
2020-04-30 20:57:06 -0600

    basic site

---
+++ web/common/footer.php
@@ -0,0 +1,1 @@
+<p>&copy; 2020 Abigail Napier | <a href="https://www.byui.edu/" target="_blank">BYU-I Student</a></p>
--- web/index.php
+++ web/index.php
@@ -9,11 +9,23 @@
 	<header>
 		<nav>
 			<?php 
-				echo $_SERVER['DOCUMENT_ROOT'];
 		        include $_SERVER['DOCUMENT_ROOT'] . '/common/nav.php';
 		    ?>
 		</nav>
+		<h1>Abi<br>Napier</h1>
+		<img src="images/me.jpg" alt="abi napier">
 	</header>
+	<main>
+		<p><span>Hey there, I’m Abi.</span>
+		I like making pretty things that make your life easier (aka. simple and beautiful sites and software). Being a developer wasn’t ever in my plan, but being a designer was, so it’s a little funny that my technical background is much more developer than designer. When I’m not programming away I’m probably making cookies, or burning off those cookies on a run. Thanks for checking out my work!
+		</p>
+		<button>Assignments</button>
+	</main>
+	<footer>
+		<?php 
+	        include $_SERVER['DOCUMENT_ROOT'] . '/common/footer.php';
+	    ?>
+	</footer>
 
 </body>
 </html>
