e771c8596b4a51c36cdfe92417c9be7a9712fc8a
abinapier <abi.napier@yahoo.com>
2020-05-08 23:12:26 -0600

    trying to fix class

--- web/candleu/common/itemClass.php
+++ web/candleu/common/itemClass.php
@@ -9,9 +9,9 @@
         public function __construct($name, $price, $description, $image) 
         { 
             $this->name = $name;
-            $this->$price = $price;
-            $this->$description = $description;
-            $this->$image = $image;
+            $this->price = $price;
+            $this->description = $description;
+            $this->image = $image;
         } 
     
     }
