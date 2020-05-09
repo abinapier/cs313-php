f6035abb36d76f78169a5de21a045b13c0e1183f
abinapier <abi.napier@yahoo.com>
2020-05-09 10:50:20 -0600

    test function

--- web/candleu/js/addItem.js
+++ web/candleu/js/addItem.js
@@ -1,5 +1,5 @@
 function addItemToCart(name, price, image, link){
-    $.ajax({ url: 'addToCart.php',
+    $.ajax({ url: '../common/addToCart.php',
             data: {'function2call': 'addToCart', 'name':name, 'price':price, 'image':image, 'link':link},
             type: 'post',
             success: function(output) {
