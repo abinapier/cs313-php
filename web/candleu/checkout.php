5f2cfef24a3b0471f82c66e621b063b12b6e0fa4
abinapier <abi.napier@yahoo.com>
2020-05-09 14:40:38 -0600

    submnit

--- web/candleu/checkout.php
+++ web/candleu/checkout.php
@@ -88,7 +88,7 @@
                     <option value="WY">Wyoming</option>
                 </select></label>
                 <label>Zip Code <input type="text" name="zip"></label>
-                <input type="submit">Place Order</input>	
+                <input type="submit" value="Place Your Order"></input>	
             </form>
             
         </main>
