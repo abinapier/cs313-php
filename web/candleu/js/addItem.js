be93b13cc051bf8d4fa0536fb39f978548a5fa83
abinapier <abi.napier@yahoo.com>
2020-05-09 13:48:17 -0600

    alert

--- web/candleu/js/addItem.js
+++ web/candleu/js/addItem.js
@@ -3,7 +3,7 @@ function addItemToCart(name, price, image, link){
             data: {'function2call': 'addToCart', 'name':name, 'price':price, 'image':image, 'link':link},
             type: 'post',
             success: function(output) {
-                alert(output);
+                
             }
     });
 }
