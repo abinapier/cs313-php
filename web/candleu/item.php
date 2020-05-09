b0bea780802277715091de3dbe2a1d69eb6b5e38
abinapier <abi.napier@yahoo.com>
2020-05-09 17:13:40 -0600

    title

--- web/candleu/item.php
+++ web/candleu/item.php
@@ -8,7 +8,7 @@
 <html lang="en">
     <head>
         <meta charset="utf-8">
-        <title></title>
+        <title><?php $_GET["name"]?></title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="stylesheet" href="css/style.css"> 
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
