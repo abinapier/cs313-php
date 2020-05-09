133b77ca25a599b88bab2498921107ba203b5b9b
abinapier <abi.napier@yahoo.com>
2020-05-09 14:05:55 -0600

    silly me no jquery

--- web/candleu/js/addItem.js
+++ web/candleu/js/addItem.js
@@ -1,3 +1,5 @@
+
+
 function addItemToCart(name, price, image, link){
     $.ajax({ url: '/candleu/common/addToCart.php',
             data: {'function2call': 'addToCart', 'name':name, 'price':price, 'image':image, 'link':link},
