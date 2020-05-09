8e5ce66d6c708e3e47f4f87ef846f1d8fa3971d1
abinapier <abi.napier@yahoo.com>
2020-05-09 13:49:58 -0600

    remove function

--- web/candleu/cart.php
+++ web/candleu/cart.php
@@ -12,6 +12,7 @@
         <title>Candleu Cart.</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="stylesheet" href="css/style.css"> 
+        <script src="js/addItem.js"></script>
     </head>
     <body>
         <?php 
