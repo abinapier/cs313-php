b5e47bdac3870eff9d26b6ea3c18196a5cd855f4
abinapier <abi.napier@yahoo.com>
2020-05-09 11:00:56 -0600

    test function

--- web/candleu/common/addToCart.php
+++ web/candleu/common/addToCart.php
@@ -10,6 +10,6 @@
 
     function addToCart($name, $price, $image, $link){
         $_SESSION["incart"] = ["name"=>$name, "price"=>$price, "image"=>$image, "link"=>$link];
-        echo($_SESSION["incart"][0]);
+        echo($_SESSION["incart"]["name"]);
     }
 ?>
