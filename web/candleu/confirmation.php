81b4a3438b6658535e65ad36438f29b34889cc4c
abinapier <abi.napier@yahoo.com>
2020-05-09 17:14:24 -0600

    title

--- web/candleu/confirmation.php
+++ web/candleu/confirmation.php
@@ -8,7 +8,7 @@
 <html lang="en">
     <head>
         <meta charset="utf-8">
-        <title>Shop Candleu.</title>
+        <title>Candleu Order Confirmation.</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="stylesheet" href="css/style.css"> 
     </head>
