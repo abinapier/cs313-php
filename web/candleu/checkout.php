cc86c155c85d21b461ff051c5219fbb963ce363f
abinapier <abi.napier@yahoo.com>
2020-05-09 16:51:57 -0600

    css

--- web/candleu/checkout.php
+++ web/candleu/checkout.php
@@ -30,10 +30,10 @@
                 echo("<span class='cost'>Total: $".$total."</span>");
             ?>
             <form action="confirmation.php" method=>
-                <label>Name <input type="text" name="name" required></label>
-                <label>Street <input type="text" name="street" required></label>
-                <label>City <input type="text" name="city" required></label>
-                <label>State <select required>
+                <label>Name <input type="text" name="name" id="name"required></label>
+                <label>Street <input type="text" name="street" id="street" required></label>
+                <label>City <input type="text" name="city" id="city" required></label>
+                <label>State <select required name="state" id="city">
                     <option value="" disabled selected>Select state</option>
                     <option value="AL">Alabama</option>
                     <option value="AK">Alaska</option>
@@ -87,7 +87,7 @@
                     <option value="WI">Wisconsin</option>
                     <option value="WY">Wyoming</option>
                 </select></label>
-                <label>Zip Code <input type="text" name="zip" required></label>
+                <label>Zip Code <input type="text" name="zip" id="zip" required></label>
                 <input type="submit" value="Place Your Order"></input>	
             </form>
             
