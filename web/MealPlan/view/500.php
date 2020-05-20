5d020e09d524a0550fa1bb31835eee8c36465176
abinapier <abi.napier@yahoo.com>
2020-05-20 09:56:56 -0600

    trying to connect

---
+++ web/MealPlan/view/500.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Server Error</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" media="screen" href="/MealPlan/css/style.css">
+</head>
+<header>
+    <?php 
+        include $_SERVER['DOCUMENT_ROOT'] . '/MealPlan/common/header.php';
+    ?>
+</header>
+
+<main>
+    <h1>Server Error</h1>
+    <p>Sorry, the server experienced a problem. </p>
+</main>
+
+<footer>
+    <?php 
+        include $_SERVER['DOCUMENT_ROOT'] . '/MealPlan/common/footer.php';
+    ?>
+</footer>
+</html>
