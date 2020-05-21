f6aca1ad7a1b4d2c21d90b5f18fb833ad291934c
abinapier <abi.napier@yahoo.com>
2020-05-20 23:38:51 -0600

    meal plan data

--- web/MealPlan/mealPlan/index.php
+++ web/MealPlan/mealPlan/index.php
@@ -12,22 +12,23 @@
 
     switch ($action){
         case 'view':
+            echo 'here!';
             $menuID = filter_input(INPUT_GET, 'id');
             $date = getMealPlanDate($menuID);
             $recipeArray = getMenuRecipes($menuID);
-
+            echo 'go';
             $recipeOneId = getRecipeName($recipeArray[0]);
             $recipeTwoId = getRecipeName($recipeArray[1]);
             $recipeThreeId = getRecipeName($recipeArray[2]);
             $recipeFourId = getRecipeName($recipeArray[3]);
             $recipeFiveId = getRecipeName($recipeArray[4]);
-
+            echo 'now';
             $recipeOneName = getRecipeName($recipeOneId);
             $recipeTwoName = getRecipeName($recipeTwoId);
             $recipeThreeName = getRecipeName($recipeThreeId);
             $recipeFourName = getRecipeName($recipeFourId);
             $recipeFiveName = getRecipeName($recipeFiveId);
-            
+
             include '../view/view-meal-plan.php';
         break;
         case 'add':
