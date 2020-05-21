a0375e7beff388bd4c5bef70ee0c571960a8af69
abinapier <abi.napier@yahoo.com>
2020-05-20 18:58:17 -0600

    adding more account functionality

--- web/MealPlan/accounts/index.php
+++ web/MealPlan/accounts/index.php
@@ -11,6 +11,8 @@
 
     switch ($action){
         case 'account':
+            echo $_SESSION['user_id'];
+            exit;
             if (isset($_SESSION['user_id'])) {
                 include '../view/account.php';
                 exit;
