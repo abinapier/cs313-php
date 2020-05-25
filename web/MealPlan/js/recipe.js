01f75a1aea502ea6034e1d1b6199f74f1b430687
abinapier <abi.napier@yahoo.com>
2020-05-25 13:05:51 -0600

    name update

--- web/MealPlan/js/recipe.js
+++ web/MealPlan/js/recipe.js
@@ -7,10 +7,10 @@ function addIngredientEntry(){
     let ingredientInput = document.getElementsByClassName("ingredient")[0];
     let cloneAmount = amountInput.cloneNode(true);
     let cloneIngredient = ingredientInput.cloneNode(true);
-    cloneAmount.children[1].value = '';
-    cloneIngredient.children[1].value = '';
-    cloneAmount.children[1].name= "amount"+count;
-    cloneIngredient.children[1].name= "ingredient"+count;
+    cloneAmount.childNodes[1].value = '';
+    cloneIngredient.childNodes[1].value = '';
+    cloneAmount.childNodes[1].name= "amount"+count;
+    cloneIngredient.childNodes[1].name= "ingredient"+count;
 
     let ingredientArea = document.getElementById("ingredientArea");
 
