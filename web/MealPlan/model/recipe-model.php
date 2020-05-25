619b90bf04768db6349bc73fbb0e9a99f404ef34
abinapier <abi.napier@yahoo.com>
2020-05-25 11:49:08 -0600

    test

--- web/MealPlan/model/recipe-model.php
+++ web/MealPlan/model/recipe-model.php
@@ -27,7 +27,7 @@
         {
             $domList.="<label>".$row['name']."<input type='checkbox' name='recipe".$row['id']."' value='".$row['id']."'></label>";    
         }
-        $domList.="<input type='text' name='action' value='delete'>">
+        $domList.="<input type='hidden' name='action' value='delete'>";
         $domList.="<input type='submit' value='Delete Recipes'>";
         $domList.="</form>";
         return $domList;
