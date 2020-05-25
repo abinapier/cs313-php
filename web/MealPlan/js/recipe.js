1e48cf5d798ef0181dbb33c212d0796649902c3f
abinapier <abi.napier@yahoo.com>
2020-05-25 12:59:12 -0600

    name update

--- web/MealPlan/js/recipe.js
+++ web/MealPlan/js/recipe.js
@@ -1,14 +1,20 @@
+var count = 1;
+
 function addIngredientEntry(){
+    count = count+1;
     let addButton = document.getElementById("addIngredient");
     let amountInput = document.getElementsByClassName("amount")[0];
     let ingredientInput = document.getElementsByClassName("ingredient")[0];
     let cloneAmount = amountInput.cloneNode(true);
-    let cloneIngredient = amountInput.cloneNode(true);
+    let cloneIngredient = ingredientInput.cloneNode(true);
     cloneAmount.firstChild.value = '';
     cloneIngredient.firstChild.value = '';
+    cloneAmount.firstChild.name= "amount"+count;
+    cloneIngredient.firstChild.name= "ingredient"+count;
 
     let ingredientArea = document.getElementById("ingredientArea");
 
     ingredientArea.insertBefore(cloneAmount, addButton);
     ingredientArea.insertBefore(cloneIngredient, addButton);
+    
 }
