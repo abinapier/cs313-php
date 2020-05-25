4260d37f83a8fb970a6af470be060246150594db
abinapier <abi.napier@yahoo.com>
2020-05-25 12:50:52 -0600

    adding js files

---
+++ web/MealPlan/js/recipe.js
@@ -0,0 +1,12 @@
+function addIngredientEntry(){
+    let addButton = document.getElementById("addIngredient");
+    let amountInput = document.getElementsByClassName("amount")[0];
+    let ingredientInput = document.getElementsByClassName("ingredient")[0];
+    let cloneAmount = amountInput.cloneNode(true);
+    let cloneIngredient = amountInput.cloneNode(true);
+    cloneAmount.firstChild.value = '';
+    cloneIngredient.firstChild.value = '';
+
+    addButton.insertBefore(cloneAmount);
+    addButton.insertBefore(cloneIngredient);
+}
