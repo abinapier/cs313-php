0ea24851e4a5bb7886d35c777b5891f602e60526
abinapier <abi.napier@yahoo.com>
2020-05-26 22:13:44 -0600

    try again?

--- web/team6/addScripture.php
+++ web/team6/addScripture.php
@@ -60,7 +60,7 @@
         echo $scriptureId;
         foreach($checkArray as $topicIndex){
             echo $topicIndex;
-            $addLink_QUERY = $db->prepare("INSERT INTO scripture_topic (scripture_id, topic_id) VALUES (:scrip_id, :topic_id");
+            $addLink_QUERY = $db->prepare("INSERT INTO scripture_topic (scripture_id, topic_id) VALUES (:scrip_id, :topic_id)");
             $addLink_QUERY->bindParam(':scrip_id', $scriptureId);
             $addLink_QUERY->bindParam(':topic_id', $topicIndex);
             $addLink_QUERY->execute();
