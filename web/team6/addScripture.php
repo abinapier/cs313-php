085d2d8b963f53b7fc91b602beb5fb23dbbf6c83
abinapier <abi.napier@yahoo.com>
2020-05-26 21:29:18 -0600

    error log

--- web/team6/addScripture.php
+++ web/team6/addScripture.php
@@ -1,4 +1,7 @@
 <?php
+    ini_set(‘display_errors’, 1);
+    ini_set(‘display_startup_errors’, 1);
+    error_reporting(E_ALL);
     $action = filter_input(INPUT_POST, 'action');
     if($action == NULL){
         $action = filter_input(INPUT_GET, 'action');
