18b8b1449b04c291490faa9b767f3ae643419082
abinapier <abi.napier@yahoo.com>
2020-05-26 21:27:21 -0600

    fix error

--- web/team6/addScripture.php
+++ web/team6/addScripture.php
@@ -27,17 +27,6 @@
     }
 
     require_once "connection.php";
-    function getScriptureTopics(){
-        $db = dbConnect();
-        $returnList = "";
-        foreach ($db->query('SELECT name, id FROM topic') as $row)
-        {
-            $returnList .= "<label>".$row["name"]."<input type='checkbox' name='topic".$row["id"]."' value='".$row["id"]."'></label>";
-    
-        }
-        return $returnList;
-
-    }
 
     function insertScripture($book, $chapter, $verse, $text, $checkArray){
         $db = dbConnect();
--- web/team6/index.php
+++ web/team6/index.php
@@ -1,5 +1,17 @@
 <?php
-    require_once "addScripture.php";
+
+    require_once "connection.php";
+    function getScriptureTopics(){
+        $db = dbConnect();
+        $returnList = "";
+        foreach ($db->query('SELECT name, id FROM topic') as $row)
+        {
+            $returnList .= "<label>".$row["name"]."<input type='checkbox' name='topic".$row["id"]."' value='".$row["id"]."'></label>";
+    
+        }
+        return $returnList;
+
+    }
     $checkboxList = getScriptureTopics();
     
 ?>
